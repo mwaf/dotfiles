3d58850e28c72ea3a958a613fb39c55a0b2ca820
Jon Krogell <mwaf@mwaf.fi>
2012-01-11 10:32:15 +0200

    Added script to init in a new environment.

---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+DOTFILES="$HOME/dotfiles"
+
+echo "source $DOTFILES/.cshrc" >> $HOME/.cshrc
+cat $DOTFILES/.gitalias >> $HOME/.gitconfig
+ln -s $DOTFILES/.gitignore_global $HOME/.gitignore_global
+ln -s $DOTFILES/.emacs $HOME/.emacs
