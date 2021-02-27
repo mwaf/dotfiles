8a2250cc09908704cd8dea5a4ca1dad396ef9b55
Jon Krogell <mwaf@mwaf.fi>
2021-02-27 14:31:23 +0200

    Fix symlink

--- init.sh
+++ init.sh
@@ -5,5 +5,5 @@ DOTFILES="$HOME/.dotfiles"
 echo "source $DOTFILES/.cshrc" >> $HOME/.cshrc
 cat $DOTFILES/.gitalias >> $HOME/.gitconfig
 ln -s $DOTFILES/.gitignore_global $HOME/.gitignore_global
-ln -s $DOTFILES/.tmux $HOME/.tmux
+ln -s $DOTFILES/.tmux.conf $HOME/.tmux.conf
 ln -s $DOTFILES/.emacs $HOME/.emacs
