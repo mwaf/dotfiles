15e836c080e9ebb5fca8d5471b9a2a0fc77cdd50
Jon Krogell <mwaf@mwaf.fi>
2023-11-01 11:54:01 +0200

    .psqlrc

--- init.sh
+++ init.sh
@@ -8,3 +8,4 @@ ln -s $DOTFILES/.gitignore_global $HOME/.gitignore_global
 ln -s $DOTFILES/.tmux.conf $HOME/.tmux.conf
 ln -s $DOTFILES/.emacs $HOME/.emacs
 ln -s $DOTFILES/.Rprofile $HOME/.Rprofile
+ln -s $DOTFILES/.psqlrc $HOME/.psqlrc
